ce0014a435e16919bffd8e6846dbb099c57e5fe4
stasp <stasprozorov54@gmail.com>
2023-09-25 16:59:30 +0200

    implemented first task

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+# 1. Користувач вводить рядок з клавіатури. Порахуйте кількість літер, цифр у рядку.
+# Виведіть обидві кількості на екран. (використовувати цикл)
 
+# try :
+#     text = str ( input ( "Enter text and numbers : " ) )
+#     numbers = 0
+#     letters = 0
+#
+#     for i in text :
+#         if i.isdigit () :
+#             numbers += 1
+#         elif i.isalpha () :
+#             letters += 1
+#
+#     print ( f"numbers - {numbers}" )
+#     print ( f"letters - {letters}" )
+#
+# except TypeError :
+#     print ( "Error" )
+#
+# except Exception as error :
+#     print ( f"Error occurred: {error}" )
