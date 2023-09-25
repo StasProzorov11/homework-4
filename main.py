caba68b20db5a76125184824ac00794f06d5df73
stasp <stasprozorov54@gmail.com>
2023-09-25 17:00:22 +0200

    implemented second task

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 
+#  2. Користувач вводить з клавіатури рядок та символ для пошуку.
+# # # Порахуйте скільки разів у рядку зустрічається потрібний символ.
+# # # Отримане число виведіть на екран.
+# try:
+#  text = str(input("Enter text and numbers : "))
+#  numbers = str(input("Enter a symbol to search : "))
+#  quantity = 0
+#
+#  for i in text:
+#     if i == numbers:
+#         quantity += 1
+#  print(f"Numbers {numbers} occurs {quantity} times")
+# except TypeError:
+#  print("Error")
+#
+# except Exception as error:
+#  print(f"Error occurred: {error}")
